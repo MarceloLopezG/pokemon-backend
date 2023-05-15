d6034043a5fabe5842ee92888f5b4a78131066e8
MarceloLopezG <mlopezguzman965@gmail.com>
2023-05-15 09:16:55 -0600

    final commit

--- src/datasources/dbconnection.datasource.ts
+++ src/datasources/dbconnection.datasource.ts
@@ -12,7 +12,10 @@ const config = {
   database: 'whatthecodedb'
 };
 
-
+// Observe application's life cycle to disconnect the datasource when
+// application is stopped. This allows the application to be shut down
+// gracefully. The `stop()` method is inherited from `juggler.DataSource`.
+// Learn more at https://loopback.io/doc/en/lb4/Life-cycle.html
 @lifeCycleObserver('datasource')
 export class DbconnectionDataSource extends juggler.DataSource
   implements LifeCycleObserver {
